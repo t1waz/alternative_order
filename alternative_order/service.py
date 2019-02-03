50614402866e41c37d86ca93e924b936e21834aa
alternative_backend <alternative_backend@wp.pl>
2019-02-03 22:02:45 +0100

    version 1.0.1

--- alternative_order/service.py
+++ alternative_order/service.py
@@ -25,7 +25,16 @@ class AppService:
         if _barcode in self.workers:
             self.my_app.worker_label = self.workers[_barcode] if not self.current_worker else '-'
             self.my_app.status_label = 'welcome' if not self.current_worker else '-'
-            self.current_worker = self.workers[_barcode] if not self.current_worker else ''
+            if not self.current_worker:
+                self.current_worker = self.workers[_barcode]
+                self.api.send_endpoint_data(endpoint='add_worker_scan',
+                                            data={"worker_barcode": self.current_worker,
+                                                  "started": True})
+            else:
+                self.api.send_endpoint_data(endpoint='add_worker_scan',
+                                            data={"worker_barcode": self.current_worker,
+                                                  "started": False})
+                self.current_worker = ''
             return True
         return False
 
@@ -47,9 +56,11 @@ class AppService:
         status, message = self.api.send_endpoint_data(endpoint='add_sended_board',
                                                       data={"board": _barcode,
                                                             "order": self.current_order})
-        self.my_app.status_label = 'ADDED' if status else message['detail']
-        if status:
+        if status == 200:
             self.current_boards.append(_barcode)
+            self.my_app.status_label = 'ADDED'
+        else:
+            self.my_app.status_label = message.get('detail', "")
 
     def handle_delete(self, _barcode):
         if self.delete_barcode(_barcode):
@@ -80,7 +91,7 @@ class AppService:
                 status, message = self.api.update_endpoint_data(
                     endpoint='orders/{}'.format(self.current_order),
                     data={"completed": "true"})
-                self.my_app.status_label = 'SENDED' if status else 'ERROR'
+                self.my_app.status_label = 'SENDED' if status == 200 else 'ERROR'
             else:
                 self.my_app.status_label = 'NOT FULL'
             self.current_boards = []
@@ -88,7 +99,7 @@ class AppService:
             self.my_app.order_id = 0
             self.my_app.order_texbox.text = ''
             self.my_app.load_order_button = 'LOAD ORDER'
-        # dopisac zeby skasowalo zamowienie 
+        # dopisac zeby skasowalo zamowienie
 
     def clear_order(self):
         self.my_app.order_detail_label = ''
@@ -113,7 +124,7 @@ class AppService:
     def delete_barcode(self, _barcode):
         status, message = self.api.delete_endpoint_data(endpoint='add_sended_board',
                                                         data={"board": _barcode})
-        if message == 'barcode removed from order' and status:
+        if message == 'barcode removed from order' and status == 200:
             self.current_boards.remove(_barcode)
             return True
         else:
@@ -144,24 +155,22 @@ class AppService:
         already_sended_boards = self.return_current_models()
 
         for index, board in enumerate(self.readed_order):
-            self.my_app.message_labels.append(
-                '{}:{}{}{}{}'.format(board,
-                                     ' ' * (20 - len(board)),
-                                     self.readed_order[board],
-                                     '        ',
-                                     already_sended_boards[board]))
+            self.my_app.message_labels.append('{}:{}{}{}{}'.format(board,
+                                                                   ' ' * (20 - len(board)),
+                                                                   self.readed_order[board],
+                                                                   '        ',
+                                                                   already_sended_boards[board]))
 
     def main_handling(self, _barcode):
         if not self.update_worker(_barcode):
-            if self.current_order == 0:
+            if not self.current_order:
                 self.my_app.status_label = 'READ ORDER'
+            elif not self.current_worker:
+                self.my_app.status_label = 'SCAN WORKER CARD'
             else:
-                if not self.current_worker == "":
-                    if self.my_app.delete_board_button == 'CANCEL DELETE':
-                        self.handle_delete(_barcode)
-                    else:
-                        self.add_barcode(_barcode)
-                    self.create_message_list()
+                if self.my_app.delete_board_button == 'CANCEL DELETE':
+                    self.handle_delete(_barcode)
                 else:
-                    self.my_app.status_label = 'SCAN WORKER CARD'
+                    self.add_barcode(_barcode)
+                self.create_message_list()
         self.update_barcode_list(_barcode)
